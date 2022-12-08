cad8a0558bdcb58f0fcf7f88127b3576c0d1efb0
Aleksandr <komptver@gmail.com>
2022-12-08 23:46:51 +0300

    Task2

---
+++ Example02_method/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача. Написать программу, которая случайного числа из отрезка от 10 до 99 включительно показывает наибольшую цифру в этом числе
+// будет 1 аргумент int берем и int и возвращаем
+
+int CutNumber(int num)
+{
+  int ed = num % 10; 
+  int des = num / 10;
+  int max = ed;
+  if (max < des) max = des;
+
+    return max;
+}
+  int randNum = new Random().Next(10, 100);
+  int newNum = CutNumber(randNum);
+  Console.WriteLine($"Из числа {randNum} большая цифра {newNum}");
+  
+
+
+
