2deeb029ab6ee8415c600063a7104442c8e13f30
Aleksandr <komptver@gmail.com>
2022-12-13 13:39:02 +0300

    added example04

---
+++ Example04_method/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Kvadrat(int num1, int num2)
+{
+    if(num1 * num1 == num2 || num2 * num2 == num1)
+       return true;
+    else
+       return false; 
+}
+bool result = Kvadrat(num1,num2);
+Console.WriteLine(($"Результат проверки: {result} "));
