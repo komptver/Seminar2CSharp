de5595dbc01e2b71fe630aa6c031b9996deb4b27
Aleksandr <komptver@gmail.com>
2022-12-13 15:01:58 +0300

    two task done

--- PracticalTask/Program.cs
+++ PracticalTask/Program.cs
@@ -1,11 +1,9 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int Threenumber(int num)
 {
    int sot = num % 100; 
-   int dec = num / 10; 
-      
-  int result = (num % 100)/10;
+   int result = sot/10;
   return result;
 }
 
@@ -14,13 +12,31 @@ int newNum = Threenumber(randNum);
                                               
 Console.WriteLine($"Outputting a thre-digit number {randNum} and show the second digit of this number {newNum}");
 
+*/
 
 
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
 /*
-int NotThree(int num)
-{
+Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
 
+int Threenumber(int num)
+  {      
+            int result = -1;
+            if (num >= 100)
+            {
+                while (num > 999)
+                {
+                    num = num / 10;
+                }
+                result = num % 10;
+            }
+          return result;
+  }
+if (Threenumber(num) == -1)
+Console.WriteLine("no third number");
+else
+Console.WriteLine($"last digit {Threenumber(num)}");
 
-}
 */
